b7906c3d761b7e1aa0e5e4675f571f0f93b20ce0
sujitacharay2005 <sujitacharya2005@gmail.com>
2020-03-22 13:11:50 +0530

    get token After token you can able to send message. Application  will be background. After sending message you get notification.

--- app/src/main/java/com/sa/fcmsampleproject/MainActivity.kt
+++ app/src/main/java/com/sa/fcmsampleproject/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.sa.fcmsampleproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val TAG = MainActivity.javaClass.simpleName;
+    }
+
+    private var mFcmToken:String?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener {
+            instanceIdResult->
+                val deviceToken = instanceIdResult.token
+            Log.i(TAG, "Success ..Token ->  $mFcmToken")
+        }
+
+        get_token.setOnClickListener{
+            Log.i(TAG, "onClick Token ->  $mFcmToken")
+        }
     }
 }
